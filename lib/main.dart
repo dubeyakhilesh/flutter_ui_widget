48d2e1df8084f9adbe0576fb06ac4f51b85dc282
Akhilesh <akhilesh2011.here@gmail.com>
2019-09-25 17:39:47 +0530

    This is the example of static listview

--- lib/main.dart
+++ lib/main.dart
@@ -27,7 +27,42 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       title: "UI Widget",
-      home: Home(),
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text("List"),
+        ),
+        body: getList(context),
+      ),
     );
+  }
+
+  Widget getList(BuildContext context){
+    var list = ListView(
+      children: <Widget>[
+        ListTile(
+          title: Text("Item"),
+          leading: Icon(Icons.airline_seat_individual_suite),
+          onTap: (){
+            //clickAction(context);
+          },
+        ),
+
+        Text("Item 2",),
+        Container(color: Colors.deepOrange, height: 50.0, margin: EdgeInsets.all(10.0),)
+      ],
+    );
+    return list;
+  }
+
+  void clickAction(BuildContext context){
+    var dialog = AlertDialog(
+      title: Text("Item"),
+      content: Text("Clicked by user"),
+    );
+
+    showDialog(context: context,
+    builder: (BuildContext context){
+      return dialog;
+    });
   }
 }
